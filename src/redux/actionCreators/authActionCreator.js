bb18395bb8c835c977cd94b19adad19eb9689b5b
chandrakommi <chandrasekhar.kommi@outlook.com>
2023-10-30 00:52:17 +0530

    show alert message for register form for errors

--- src/redux/actionCreators/authActionCreator.js
+++ src/redux/actionCreators/authActionCreator.js
@@ -18,19 +18,39 @@ const logoutUser = () => {
 const signInUser = (email, password) => dispatch => {
   auth
     .signInWithEmailAndPassword(email, password)
-    .then(user => {
-      const { email, uid } = user?.user
-      console.log('success', email, uid)
+    .then(profile => {
+      const { uid, displayName, email } = profile.user
+      dispatch(loginUser({ uid: uid, name: displayName || '', email: email }))
     })
     .catch(err => console.log(err))
 }
 
+const error_codes_messages = {
+  'auth/email-already-in-use':
+    'The email address is already in use by another account',
+  'auth/weak-password': 'Password should be at least 6 characters',
+  'auth/invalid-email': 'The email address is badly formatted.',
+}
+
 const signUpUser = (email, name, password) => async dispatch => {
   try {
-    auth.createUserWithEmailAndPassword(email, password)
-    const currentUser = auth.currentUser
-    currentUser.updateProfile({ displayName: name })
-    console.log(currentUser)
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        const currentUser = auth.currentUser
+        currentUser.updateProfile({ displayName: name })
+        dispatch(
+          loginUser({
+            uid: currentUser.uid,
+            name: currentUser.displayName,
+            email: currentUser.email,
+          })
+        )
+      })
+      .catch(error => {
+        console.log(error)
+        alert(error_codes_messages[error.code] || 'Error occured')
+      })
   } catch (error) {
     console.error('Sign-up error:', error)
   }
